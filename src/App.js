268fc22fe46382b1a8fd9ed7bb24d1d15efb7b26
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-01 00:21:30 +0600

    another file create for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Countries from './components/Country/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
+      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
 
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+
+  }, [])
+  return (
+    <div>
+      <h1>Visiting Every coutry of the world</h1>
+      <h3>Available countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population} flag={country.flags.png} capital={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    // style={{border: '2px solid gray', margin:'10px'}}
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+      <p>Capital: {props.capital}</p>
+      <img src={props.flag} alt="" />
+    </div>
+  )
+}
 export default App;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h4>This is person components</h4>
+        </div>
+    );
+};
+
+export default Person;
